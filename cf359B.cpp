d1d4cc851c7f646bec1b9d188403600ed8377af9
Suraj Bora <surajbora021@gmail.com>
2016-07-27 23:37:04 +0530

    committed

---
+++ cf359B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n, k;
+
+    cin >> n >> k;
+
+    for( int i= 1; i<= k; ++i ){
+        cout << (2*i) << ' ' << (2*i-1) << ' ';
+    }
+
+    for( int i= k+1; i<= n; ++i )
+        cout << (2*i-1) << ' ' << ( 2*i ) << ' ' ;
+
+    return 0;
+}
